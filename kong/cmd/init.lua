1ea32d1a5434b1a7aa2b7c02bbbdf6e1c03d5f1f
Zhefeng Chen <chzf_zju@163.com>
2023-07-18 14:46:24 +0800

    remove the unused contents after solving conflict

--- kong/cmd/init.lua
+++ kong/cmd/init.lua
@@ -4,7 +4,6 @@ math.randomseed() -- Generate PRNG seed
 
 local pl_app = require "pl.lapp"
 local log = require "kong.cmd.utils.log"
-local inject_directives = require "kong.cmd.utils.inject_directives"
 
 local function stop_timers()
   -- shutdown lua-resty-timer-ng to allow the nginx worker to stop quickly
@@ -22,14 +21,6 @@ return function(cmd_name, args)
     log.set_lvl(log.levels.verbose)
   elseif args.vv then
     log.set_lvl(log.levels.debug)
-  end
-
-  -- inject necessary nginx directives (e.g. lmdb_*, lua_ssl_*)
-  -- into the temporary nginx.conf that `resty` will create
-  if inject_cmds[cmd_name] and not args.no_inject then
-    log.verbose("start to inject nginx directives and respawn")
-    inject_directives.respawn(cmd_name, args)
-    return
   end
 
   log.verbose("Kong: %s", _KONG._VERSION)
